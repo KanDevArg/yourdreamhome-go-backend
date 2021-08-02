a35f3d46fa267b459845f84fcd34ce66b32bbdf2
Sebas Navarro <sebanavarro@gmail.com>
2021-08-02 18:18:35 -0300

    sorting

--- domain/listing.go
+++ domain/listing.go
@@ -3,11 +3,12 @@ package domain
 import (
 	"fmt"
 	"github.com/KanDevArg/yourdreamhome/go-backend/utils"
+	"sort"
 )
 
 var (
 	listings = map[string]*Listing{
-		"5001": {Id: 1, Address: "Cesar Carrizo 2982", City: "Cordoba", PostCode: "5001"},
+		"5007": {Id: 1, Address: "Cesar Carrizo 2982", City: "Cordoba", PostCode: "5007"},
 		"5002": {Id: 2, Address: "Loka loka 2001", City: "Cordoba", PostCode: "5002"},
 		"5003": {Id: 3, Address: "Colodrero 6001", City: "Cordoba", PostCode: "5003"},
 	}
@@ -27,8 +28,17 @@ func GetListingsByPostalCode(postalCode string) (*Listing, error) {
 
 func GetAllListings() []Listing {
 	var arr []Listing
-	for _, v := range listings {
-		arr = append(arr, *v)
+	var keys []string
+
+	for k := range listings {
+		keys = append(keys, k)
+	}
+
+	//sort.Sort(sort.Reverse(sort.Strings(keys))
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		arr = append(arr, *listings[k])
 	}
 	return arr
 }
